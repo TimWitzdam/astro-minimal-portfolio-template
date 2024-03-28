f02392e8c50e3f49ea45603cee36c3dd515b2b4d
timwitzdam <tim2013w@gmail.com>
2024-03-28 19:59:17 +0100

    Added header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-bg": "#2E2E2E",
+      },
+    },
+  },
+  plugins: [],
+};
