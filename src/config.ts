f0a45b0c4458a68e6934e76936e414ce3738e6c0
timwitzdam <tim2013w@gmail.com>
2024-03-29 10:49:49 +0100

    Added fully configureable top section

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,16 @@
-import type { NavBarLink } from "./types/config";
+import type {
+  NavBarLink,
+  SocialLink,
+  HomePageLink,
+  Identity,
+} from "./types/config";
+
+export const identity: Identity = {
+  name: "Tim Witzdam",
+  role: "Student & Software Developer",
+  description:
+    " I'm Tim Witzdam, a full time student from Germany who also loves building cool things using code.",
+};
 
 export const navBarLinks: NavBarLink[] = [
   {
@@ -16,5 +28,30 @@ export const navBarLinks: NavBarLink[] = [
   {
     title: "Blog",
     url: "/blog",
+  },
+];
+
+export const socialLinks: SocialLink[] = [
+  {
+    title: "GitHub",
+    url: "https://github.com/TimWitzdam",
+    icon: "mdi:github",
+    external: true,
+  },
+  {
+    title: "Mail",
+    url: "mailto:tim@witzdam.com",
+    icon: "mdi:email",
+  },
+];
+
+export const homePageLinks: HomePageLink[] = [
+  {
+    title: "My Projects",
+    url: "/projects",
+  },
+  {
+    title: "About Me",
+    url: "/about",
   },
 ];
--- src/types/config.ts
+++ src/types/config.ts
@@ -2,3 +2,22 @@ export type NavBarLink = {
   title: string;
   url: string;
 };
+
+export type SocialLink = {
+  title: string;
+  url: string;
+  icon: string;
+  external?: boolean;
+};
+
+export type HomePageLink = {
+  title: string;
+  url: string;
+  external?: boolean;
+};
+
+export type Identity = {
+  name: string;
+  role: string;
+  description: string;
+};
