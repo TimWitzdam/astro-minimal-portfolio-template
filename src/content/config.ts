9e6de12ffa50677453e5442c09ff0f1f085c49cb
timwitzdam <tim2013w@gmail.com>
2024-03-29 12:46:51 +0100

    Added blog section

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
---
+++ src/content/posts/Test.md
@@ -0,0 +1,7 @@
+---
+title: "My First Blog Post with a very long name"
+pubDate: 2022-07-01 #Y-M-D
+description: "Test"
+author: "Tim"
+image: { url: "/logo.webp", alt: "Test" }
+---
