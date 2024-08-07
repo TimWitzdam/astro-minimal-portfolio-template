24275d8d94c4810e24d40f9d0689c54033d9c3b9
MrJoy05 <alexander_aelgria@outlook.com>
2024-08-06 20:30:12 -0600

    despliegue a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
