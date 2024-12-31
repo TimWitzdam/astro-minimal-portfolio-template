97c268db49120c1dd545b3e3aae858ff2a15a102
TimWitzdam <tim2013w@gmail.com>
2024-12-31 14:03:48 +0100

    Update to Astro v5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,10 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/posts" }),
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
